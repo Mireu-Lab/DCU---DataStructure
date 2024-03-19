12236fc43a26a0d6affe6853e3be9f242d71514e
Mireu Lim <limmireu1214@gmail.com>
2024-03-19 04:46:56 +0000

    build or file upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DCU-DataStructure
 학교 수업 자료구조
+
+## 실습환경
+- GCC version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.2) 
--- Test2-2.cpp
+++ Test2-2.cpp
@@ -2,25 +2,40 @@
 #include <map>
 using namespace std;
 
-map<int, int, char> m = {
-    {1, 91, "a"}, 
-    {2, 86, "b"}, 
-    {5, 97, "a"}
-};
+void index_program(){
+    int grades[3] = {91, 86, 97};
+    char rating[3] = {'a', 'b', 'a'};
 
+    for (int i = 0; i <= 2; i+=1) {
+        printf("학년: %d 학점: %d 등급: %c\n", i+1, grades[i], rating[i]);
+    }
+
+    return;
+}
+
+
+void format_program() {
+    struct infoFormat {
+        int grades;
+        char rating;
+    };
+
+    struct infoFormat infoData[3] = {{91, 'a'}, {86, 'b'}, {97, 'a'}};
+
+    for (int i = 0; i <= 2; i+=1) {
+        printf("학년: %d 학점: %d 등급: %c\n", i+1, infoData[i].grades, infoData[i].rating);
+    }
+
+    return;
+}
 
 int main() {
+    
+    index_program();
 
-    // for (int i = 0; i > m.size(); i+=1) {
-    //     printf("학년: %s, 학점: %s 등급: %s", m->);
-    // }
+    cout << "==================" << endl;
 
-    auto iter = m.begin();
-    while (iter != tempMap.end()) {
-        cout << "[" << iter->first << "," << iter->second << "]\n";
-        ++iter;
-    }
-    cout << endl;
+    format_program();
 
-    return 0
+    return 0;
 }
