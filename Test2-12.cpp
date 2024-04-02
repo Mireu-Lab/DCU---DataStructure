8d216ba54f81d1c8b9828a1998f82f8952f1db11
Mireu Lim <limmireu1214@gmail.com>
2024-04-02 00:36:35 +0000

    feat: create Test2-12.cpp and Test2-12

---
+++ Test2-12.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct PayInfo {
+    char Name[10];
+    int Year;
+    int Pay;
+};
+
+
+int main() {
+    PayInfo payData[3] = {
+        {"임미르", 2023, 0001},
+        {"임미르", 2024, 0001},
+        {"임미르", 2021, 0001}
+    };
+
+    for (int a = 0; a < 3; a+=1) {
+        printf("이름: %s\n\t입사: %d\n\t연봉: %d\n", 
+                payData[a].Name, 
+                payData[a].Year, 
+                payData[a].Pay
+            );
+    }
+}
+
